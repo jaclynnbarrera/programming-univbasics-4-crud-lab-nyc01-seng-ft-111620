6be4311f0bfb198f18b75e4080dc79cf349620b2
jaclyn barrera <jaclynnbarrera@gmail.com>
2020-11-09 15:12:07 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -29,7 +29,8 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  four = ["am", "I", "alive"]
+  four[0]
 end
 
 def retrieve_first_element_from_array(array)
