fd4c855de9074106f737ff961db8184ae1ada27f
jaclyn barrera <jaclynnbarrera@gmail.com>
2020-11-09 15:09:11 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -20,7 +20,7 @@ end
 
 def remove_element_from_end_of_array(array)
   four = ["one", "two", "three", "four"]
-  four.pop[-1]
+  four.pop["four"]
 end
 
 def remove_element_from_start_of_array(array)
