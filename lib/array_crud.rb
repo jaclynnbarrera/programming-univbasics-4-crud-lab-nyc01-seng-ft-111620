3eb6de567075d0d8539acfaebbfa8b4bde732436
jaclyn barrera <jaclynnbarrera@gmail.com>
2020-11-09 15:13:09 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -39,7 +39,8 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-  
+  four = ["wow", "I", "alive"]
+  four[-1]
 end
 
 def update_element_from_index(array, index_number, element)
